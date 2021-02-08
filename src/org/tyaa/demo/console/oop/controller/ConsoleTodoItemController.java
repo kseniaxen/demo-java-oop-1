a8686ba51452a7b227265ce72b1087c52c695660
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-02-08 17:35:53 +0200

    New fields for TodoItem

--- src/org/tyaa/demo/console/oop/controller/ConsoleTodoItemController.java
+++ src/org/tyaa/demo/console/oop/controller/ConsoleTodoItemController.java
@@ -3,6 +3,10 @@ package org.tyaa.demo.console.oop.controller;
 import org.tyaa.demo.console.oop.model.TodoItem;
 import org.tyaa.demo.console.oop.repository.TodoItemRepository;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,7 +23,24 @@ public final class ConsoleTodoItemController extends AbstractTodoItemController 
         public void create() {
             System.out.println("Input title then press Enter");
             String title = scanner.nextLine();
-            TodoItem item = new TodoItem(title);
+            System.out.println("Input description then press Enter");
+            String description = scanner.nextLine();
+
+            System.out.println("Input date then press Enter");
+            String date = scanner.nextLine();
+            DateFormat dateFormat=new SimpleDateFormat("dd.MM.yyyy");
+            Date dateParse = null;
+            try {
+                dateParse = dateFormat.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Input done then press Enter");
+            String done = scanner.nextLine();
+            Boolean doneParse = Boolean.parseBoolean(done);
+
+            TodoItem item = new TodoItem(title,description,dateParse,doneParse);
             todoItemRepository.add(item);
             System.out.println(Constants.PLACEHOLDER_SYMBOLS);
         }
--- src/org/tyaa/demo/console/oop/model/TodoItem.java
+++ src/org/tyaa/demo/console/oop/model/TodoItem.java
@@ -1,11 +1,17 @@
 package org.tyaa.demo.console.oop.model;
 
+import javax.swing.text.StyledEditorKit;
+import java.util.Date;
+
 public class TodoItem {
 
     private static Integer lastId;
 
     private Integer id;
     private String title;
+    private String description;
+    private Date date;
+    private Boolean done;
 
     static {
         lastId = 0;
@@ -15,14 +21,20 @@ public class TodoItem {
         this.id = ++lastId;
     }
 
-    public TodoItem(Integer id, String title) {
+    public TodoItem(Integer id, String title, String description, Date date, Boolean done) {
         this.id = id;
         this.setTitle(title);
+        this.setDescription(description);
+        this.setDate(date);
+        this.setDone(done);
     }
 
-    public TodoItem(String title) {
+    public TodoItem(String title, String description, Date date, Boolean done) {
         this.id = ++lastId;
         this.setTitle(title);
+        this.setDescription(description);
+        this.setDate(date);
+        this.setDone(done);
     }
 
     public Integer getId() {
@@ -40,11 +52,38 @@ public class TodoItem {
         this.title = title;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
     @Override
     public String toString() {
         return "TodoItem{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", date='" + date + '\'' +
+                ", done='" + done + '\'' +
                 '}';
     }
 }
